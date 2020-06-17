129b218322428d56967043403316476a12605cc2
alenaalena512 <48884708+alenaalena512@users.noreply.github.com>
2020-06-17 16:47:38 +0100

    www

--- www/server.js
+++ www/server.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -14,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const util_1 = require("./util/util");
-(() => __awaiter(this, void 0, void 0, function* () {
+(() => __awaiter(void 0, void 0, void 0, function* () {
     // Init the Express application
     const app = express_1.default();
     // Set the network port
@@ -35,7 +36,7 @@ const util_1 = require("./util/util");
     // RETURNS
     //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
     /**************************************************************************** */
-    app.get("/filteredimage", (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.get("/filteredimage", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const { image_url } = req.query;
         const filteredPath = yield util_1.filterImageFromURL(image_url);
         if (!image_url)
@@ -54,7 +55,7 @@ const util_1 = require("./util/util");
     }));
     // Root Endpoint
     // Displays a simple message to the user
-    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.send("try GET /filteredimage?image_url={{}}");
     }));
     // Start the Server
